071ca27af1929adb884e7294a20b92a703c03a23
hahuynhvan <hahuynhhuy159@gmail.com>
2025-11-13 14:09:17 +0700

    13/11

--- 02-spring-boot-aop-demo-pointcut-declarations - Copy/src/main/java/com/vanhuynh/aopdemo/dao/AccountDAO.java
+++
@@ -1,8 +0,0 @@
-package com.vanhuynh.aopdemo.dao;
-
-import com.vanhuynh.aopdemo.Account;
-
-public interface AccountDAO {
-    void addAccount(Account theAccount, boolean vigFlag);
-    boolean doWork();
-}
--- 02-spring-boot-aop-demo-pointcut-declarations - Copy/src/main/java/com/vanhuynh/aopdemo/dao/MembershipDAO.java
+++
@@ -1,6 +0,0 @@
-package com.vanhuynh.aopdemo.dao;
-
-public interface MembershipDAO {
-    boolean addSillyMember();
-    void goToSleep();
-}
